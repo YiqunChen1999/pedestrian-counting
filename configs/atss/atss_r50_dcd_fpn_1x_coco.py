bf31f925dc396388f321bc8b14fcf69031969d3e
The pratice about ML <70280218+weihai-98@users.noreply.github.com>
2022-05-10 22:05:48 +0800

    Update atss_r50_dcd_fpn_1x_coco.py

--- configs/atss/atss_r50_dcd_fpn_1x_coco.py
+++ configs/atss/atss_r50_dcd_fpn_1x_coco.py
@@ -18,7 +18,16 @@ model = dict(
         style='pytorch',
         init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
     neck=dict(
-        type='FPN_dcd'),
+        type='FPN_dcd',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=256,
+        start_level=1,
+        add_extra_convs='on_output',
+        num_outs=5,
+        # K代表每层中，动态卷积的次数
+        # t代表softmax函数中的温度
+        K=4,
+        t=30),
     bbox_head=dict(
         type='ATSSHead',
         num_classes=1,
