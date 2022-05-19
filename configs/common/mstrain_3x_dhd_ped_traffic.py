0cd66d90913b101757b6cd9b67299844364f6ae1
yiqunchen1999 <yiqunchen1999@163.com>
2022-05-19 21:42:00 +0800

    add mstrain_3x for deform conv

--- configs/common/mstrain_3x_dhd_ped_traffic.py
+++ configs/common/mstrain_3x_dhd_ped_traffic.py
@@ -39,7 +39,7 @@ test_pipeline = [
 
 # Use RepeatDataset to speed up training
 data = dict(
-    samples_per_gpu=2,
+    samples_per_gpu=4,
     workers_per_gpu=2,
     train=dict(
         type='RepeatDataset',
