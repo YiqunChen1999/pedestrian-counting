2321e6812699f5b28b0a2c839f577ba1945c9653
yiqunchen1999 <yiqunchen1999@163.com>
2022-05-20 14:10:41 +0800

    add code and result of deform conv

---
+++ configs/atss/atss_r50_fpn_deform_head_4x_dhd_ped_traffic.py
@@ -0,0 +1,8 @@
+_base_ = [
+    '../_base_/datasets/dhd_ped_traffic.py',
+    '../_base_/models/atss_r50_fpn_deform_head.py',
+    '../_base_/schedules/schedule_4x.py', 
+    '../_base_/default_runtime.py', 
+]
+# optimizer
+optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0001)
